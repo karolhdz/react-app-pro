28cba276a61ca6ddd48f09f33ada4c556666115b
Karol Olg Hdz <karolhe@gmail.com>
2022-07-20 11:30:24 -0500

    Inicia seccion 8

--- src/02-component-patterns/pages/ShoopingPage.tsx
+++ src/02-component-patterns/pages/ShoopingPage.tsx
@@ -1,73 +1,34 @@
 import { ProductButtons, ProductImage, ProductTitle, ProductCard } from '../components';
 import { products } from '../data/products';
 import '../styles/custom-styles.css'
-import { useShoppinCard } from '../hooks/useShoppingCard';
+
+const product = products[0]
 
 export const ShoopingPage = () => {
 
-    const {onProductCountChange, shoppingCart} = useShoppinCard();
+    return (
+        <div>
+            <h1>Shopping Store</h1>
+            <hr />
 
-return (
-    <div>
-        <h1>Shopping Store</h1>
-        <hr />
 
-        <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap'
-        }}>
 
-            {
-                products.map(pro => (<ProductCard key={pro.id}
-                    className='bg-dark text-white'
-                    product={pro} 
-                    onChange={onProductCountChange}
-                    value={ shoppingCart[pro.id]?.count || 0  }
-                >
-                    <ProductImage className='custom-image' />
-                    <ProductTitle />
-                    <ProductButtons className='custom-buttons' />
+            <ProductCard key={product.id}
+                className='bg-dark text-white'
+                product={product}
 
-                </ProductCard>))
-            }
-            {/* <ProductCard product={product}
-                    className='bg-dark text-white'
-                >
-                    <ProductCard.Image className='custom-image'/>
-                    <ProductCard.Title className='text-white'/>
-                    <ProductCard.Buttons className='custom-buttons'/>
+            >
+                <ProductImage className='custom-image' />
+                <ProductTitle />
+                <ProductButtons className='custom-buttons' />
 
-                </ProductCard> */}
+            </ProductCard>
+
+
+
 
         </div>
-
-        <div className='shopping-cart'>
-            {
-                Object.entries(shoppingCart).map(([key, prodInCart]) => (
-                    <ProductCard product={prodInCart}
-                        className='bg-dark text-white'
-                        style={{ width: '100px' }}
-                        key={key}
-                        value={prodInCart.count}
-                        onChange={onProductCountChange}
-                    >
-                        <ProductCard.Image className='custom-image' />
-                        <ProductTitle />
-                        <ProductCard.Buttons className='custom-buttons'
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center'
-                            }}
-                        />
-
-                    </ProductCard>
-                ))
-            }
-        </div>
-
-    </div>
-)
+    )
 }
 
 export default ShoopingPage
