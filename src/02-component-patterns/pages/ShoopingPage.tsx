614149fd9d71160ebba162cab29ca940c88cf797
Karol Olg Hdz <karolhe@gmail.com>
2022-07-21 18:06:05 -0500

    Fin seccion 9

--- src/02-component-patterns/pages/ShoopingPage.tsx
+++ src/02-component-patterns/pages/ShoopingPage.tsx
@@ -12,28 +12,18 @@ export const ShoopingPage = () => {
             <hr />
             <ProductCard
                 key={product.id}
-                className='bg-dark text-white'
                 product={product}
                 initialValues={{
                     count: 4,
-                    maxCount: 10,
+                    // maxCount: 10,
                 }}
             >
                 {
                     ({ reset, count, onHandleIncrease, isMaxCountReached }) => (
                         <>
-                            <ProductImage className='custom-image' />
+                            <ProductImage/>
                             <ProductTitle />
-                            <ProductButtons className='custom-buttons' />
-                            <button onClick={reset}>reset</button>
-                            <button onClick={() => onHandleIncrease(-2)} > -2  </button>
-                            <span> {count} </span>
-                            {
-                                !isMaxCountReached && (<button onClick={() => onHandleIncrease(2)}
-                                    className={isMaxCountReached ? 'display: none' : ''}
-                                >
-                                    +2 </button>)
-                            }
+                            <ProductButtons/>
 
                         </>
                     )
